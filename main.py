85dcda7ae975dfba7c9e97068fe3c73016d61e68
vodaiphuoc <phuocvodn98@gmail.com>
2026-01-17 10:19:36 +0700

    update queue

--- main.py
+++ main.py
@@ -9,13 +9,20 @@ load_dotenv()
 
 TTS_URL = os.environ['DEPLOY_DOMAIN']
 
-client = genai.Client(api_key=os.environ['API'], http_options={"api_version": "v1alpha"})
+client = genai.Client(api_key=os.environ['GEMINI_API'], http_options={"api_version": "v1alpha"})
 MODEL = "gemini-2.5-flash-native-audio-preview-12-2025"
+
+
+SYSTEM_INST = """
+Bạn là một voicebot thân thiện với con người, hãy luôn trả lời câu hỏi một
+cách vui vẻ, và bằng tiếng Việt (Vietnamese)
+"""
 
 CONFIG = types.LiveConnectConfig(
     response_modalities=["AUDIO"],
     output_audio_transcription = {},
-    enable_affective_dialog = True
+    enable_affective_dialog = True,
+    system_instruction = SYSTEM_INST
 )
 
 pya = pyaudio.PyAudio()
@@ -26,9 +33,10 @@ SEND_SAMPLE_RATE = 16000
 RECEIVE_SAMPLE_RATE = 24000
 CHUNK_SIZE = 1024
 
-
-audio_queue_output = asyncio.Queue()
 audio_queue_mic = asyncio.Queue(maxsize=5)
+audio_queue_output = asyncio.Queue(maxsize=20)
+
+PCM_CHUNK_BYTES = CHUNK_SIZE * 2  # int16 = 2 bytes
 
 
 async def listen_audio():
@@ -63,33 +71,38 @@ async def receive_live_response(live_session, tts_ws):
         async for response in turn:
             if response.server_content.output_transcription:
                 response_segment_text = response.server_content.output_transcription.text
-                tts_ws.send(response_segment_text)
+                print('receive response_segment_text: ',response_segment_text)
+                await tts_ws.send(response_segment_text)
 
 async def receive_audio_response(tts_ws):
     """Receives responses from TTS service"""
     while True:
         audio_response = await tts_ws.recv()
+        print('receive audio response: ', type(audio_response))
         audio_queue_output.put_nowait(audio_response)
-
-        # Empty the queue on interruption to stop playback
-        while not audio_queue_output.empty():
-            audio_queue_output.get_nowait()
 
 async def play_audio():
     """Plays audio from the speaker audio queue."""
     speaker_info = pya.get_default_output_device_info()
-    print(speaker_info)
+    pcm_buffer = bytearray()
     stream = await asyncio.to_thread(
         pya.open,
         format=FORMAT,
         channels=CHANNELS,
         rate=RECEIVE_SAMPLE_RATE,
         output=True,
-        output_device_index = speaker_info['index']
+        output_device_index = speaker_info['index'],
+        frames_per_buffer=CHUNK_SIZE,
     )
     while True:
-        bytestream = await audio_queue_output.get()
-        await asyncio.to_thread(stream.write, bytestream)
+        data = await audio_queue_output.get()
+        pcm_buffer.extend(data)
+
+        while len(pcm_buffer) >= PCM_CHUNK_BYTES:
+            chunk = pcm_buffer[:PCM_CHUNK_BYTES]
+            del pcm_buffer[:PCM_CHUNK_BYTES]
+
+            await asyncio.to_thread(stream.write, bytes(chunk))
 
 async def run():
     """Main function to run the audio loop."""
@@ -102,6 +115,7 @@ async def run():
                 listen_audio(),
                 send_realtime(live_session),
                 receive_live_response(live_session, tts_ws),
+                receive_audio_response(tts_ws),
                 play_audio()
             )
             
