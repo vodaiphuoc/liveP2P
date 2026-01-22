8948596c75472ea08d12589a313e1a9ed4c994e7
vodaiphuoc <phuocvodn98@gmail.com>
2026-01-22 20:01:51 +0700

    edit args

--- finetuning/convert/export.sh
+++ finetuning/convert/export.sh
@@ -3,7 +3,6 @@
 OPTS=$(getopt -o "" --long merged_output_path: -- "$@")
 eval set -- "$OPTS"
 
-
 git clone https://github.com/ggerganov/llama.cpp
 
 echo "install dependecies"
@@ -12,5 +11,5 @@ cd llama.cpp && pip install -r requirements.txt
 echo "Building llama.cpp"
 make -j$(nproc)
 
-python llama.cpp/convert_hf_to_gguf.py "$merged_output_path" --outfile model-fp16.gguf --outtype f16 && \
+python llama.cpp/convert_hf_to_gguf.py "$2" --outfile model-fp16.gguf --outtype f16 && \
 llama.cpp/quantize model-fp16.gguf model-q4.gguf Q4_K_M
