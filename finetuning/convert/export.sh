e897b305312a3c23dfd705847db8fff7ab736acb
vodaiphuoc <phuocvodn98@gmail.com>
2026-01-22 20:31:18 +0700

    fix path

--- finetuning/convert/export.sh
+++ finetuning/convert/export.sh
@@ -6,10 +6,10 @@ eval set -- "$OPTS"
 git clone https://github.com/ggerganov/llama.cpp
 
 echo "install dependecies"
-cd llama.cpp && pip install -r requirements.txt
+cd llama.cpp && pip install -r requirements.txt --quiet
 
 echo "Building llama.cpp"
 make -j$(nproc)
 
-python ./llama.cpp/convert_hf_to_gguf.py "$2" --outfile model-fp16.gguf --outtype f16 && \
+python convert_hf_to_gguf.py "$2" --outfile model-fp16.gguf --outtype f16 && \
 ./llama.cpp/quantize model-fp16.gguf model-q4.gguf Q4_K_M
