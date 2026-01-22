b4e54e84e6484fbfe67607396abe6092ce7fdb99
vodaiphuoc <phuocvodn98@gmail.com>
2026-01-22 15:25:19 +0700

    fix traing args for oom

--- finetuning/train.py
+++ finetuning/train.py
@@ -138,8 +138,8 @@ training_config = {
     
     # --- CẤU HÌNH CHO TUAL T4 ---
     'per_device_eval_batch_size': 1,
-    'per_device_train_batch_size': 1,   # Giữ là 1 để an toàn vì VRAM T4 (15GB) < P100 (16GB)
-    'gradient_accumulation_steps': 4,   # Tăng lên 8 (để bù lại batch size nhỏ)
+    'per_device_train_batch_size': 2,   # Giữ là 1 để an toàn vì VRAM T4 (15GB) < P100 (16GB)
+    'gradient_accumulation_steps': 8,   # Tăng lên 8 (để bù lại batch size nhỏ)
     # ----------------------------
     
     'learning_rate': 2e-4,
